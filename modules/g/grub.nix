b475718b690e5b24efd5aac0da1f475828a56558
Franklin Azriel <franklinazriel6942@outlook.com>
2024-08-01 22:13:25 +0700

    refactor: Rewrite code of grub.nix

--- modules/g/grub.nix
+++ modules/g/grub.nix
@@ -1,21 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader = {
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-      };
-      grub = {
-        efiSupport = true;
-        #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-        device = "nodev";
-      };
-    };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+  
+  boot.loader.grub.efiSupport = true;
+  #boot.loader.grub.efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+  boot.loader.grub.device = "nodev";
 
   boot.extraModprobeConfig = ''
+  # AMD KVM Nested Virtualization
   options kvm_amd nested=1
   
+  # Realtek RTW89
   options rtw89_pci disable_aspm_l1=y disable_aspm_l1ss=y
   options rtw89pci disable_aspm_l1=y disable_aspm_l1ss=y
   options rtw89_core disable_ps_mode=y
