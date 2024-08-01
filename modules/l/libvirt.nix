14247b42722dd2cad6beafb5df2550033970774b
Franklin Azriel <franklinazriel6942@outlook.com>
2024-08-01 22:14:54 +0700

    refactor: Rewrite code of libvirt.nix

--- modules/l/libvirt.nix
+++ modules/l/libvirt.nix
@@ -1,19 +1,11 @@
 { config, pkgs, ... }:
 
 {
-    virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-        }).fd];
-        };
-    };
-    };
+    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
+    virtualisation.libvirtd.qemu.runAsRoot = true;
+    virtualisation.libvirtd.qemu.swtpm.enable = true;
+    virtualisation.libvirtd.qemu.ovmf.enable = true;
+    virtualisation.libvirtd.qemu.ovmf.packages = [];
+
 }
