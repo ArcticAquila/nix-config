6a394636ddb56f13824f031b680b78a8de4494d1
Franklin Azriel <franklinazriel6942@outlook.com>
2024-08-01 21:56:27 +0700

    fix: Fixing modprobe syntax for wireless RTW89

--- modules/g/grub.nix
+++ modules/g/grub.nix
@@ -13,5 +13,12 @@
       };
     };
 
-  boot.extraModprobeConfig = "options kvm_amd nested=1";
+  boot.extraModprobeConfig = ''
+  options kvm_amd nested=1
+  
+  options rtw89_pci disable_aspm_l1=y disable_aspm_l1ss=y
+  options rtw89pci disable_aspm_l1=y disable_aspm_l1ss=y
+  options rtw89_core disable_ps_mode=y
+  options rtw89core disable_ps_mode=y
+'';
 }
